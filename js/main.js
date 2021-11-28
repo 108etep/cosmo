0535c2862043b65b639e4439094653d1aa5437b4
Yuzhnii <70344381+108etep@users.noreply.github.com>
2021-11-28 23:19:45 +0500

    adapt

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
 $(function(){
-    $('.slider__box').slick({
-        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
-        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
-        responsive: [
-            {
-              breakpoint: 481,
-              settings: {
-                arrows:false,
-              }
-            },
-            
-          ]
-        });
-        
-    
-
-
-    $('.menu-btn').on('click', function(){
-        $('.menu__list').toggleClass('active');
-    });
+	$('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+	$('.gallery__slider').slick({
+		prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+		nextArrow:'<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+	});
+	$('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		
+		
+	});
+	$('.menu__btn').on('click', function(){
+		$('.menu__list').toggleClass('menu__list--active')
+	});
 });
